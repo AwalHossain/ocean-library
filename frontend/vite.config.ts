0483c69fff53124af9c48da2bf9d539e0e2fc5ed
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-07-24 03:56:03 +0000

    🌱 Initial commit: Begin development with basic features 🚀

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+})
