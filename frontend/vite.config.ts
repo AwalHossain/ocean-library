c8fe5ebf40f858d1dab477ccb975bcaa6c77876c
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2024-04-20 14:42:53 +0000

    Update Vite configuration to use port 3000

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,14 @@
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:'127.0.0.1',
-    port:5173,
+  server: {
+    port: 3000,
   },
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
-})
+});
