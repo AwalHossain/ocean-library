1e74469445b88daf54aaa72fcd9384c0dc3cb737
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2024-04-20 10:06:16 +0000

    Update Container component styling and export in container.tsx

--- frontend/src/components/container.tsx
+++ frontend/src/components/container.tsx
@@ -1,21 +1,20 @@
-
 import { ReactNode } from "react";
 
 type ContainerProps = {
-    className?: string;
-    children: ReactNode;
-  };
-  
+  className?: string;
+  children: ReactNode;
+};
 
 const Container = (props: ContainerProps) => {
   return (
     <div
-      className={`container p-8 mx-auto xl:px-0 ${
+      className={`md:container justify-center items-center md:p-8 mx-auto xl:px-0 ${
         props.className ? props.className : ""
-      }`}>
+      }`}
+    >
       {props.children}
     </div>
   );
-}
+};
 
 export default Container;
