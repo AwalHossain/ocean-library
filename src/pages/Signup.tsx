4c5066614286907df552d1677f3ad86ebe39caec
awalhossain <waqin21@gmail.com>
2024-04-25 02:51:26 +0600

    Update location handling

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,4 @@
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // import Image from "next/image";
 import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
@@ -11,121 +11,165 @@ import { useAppSelector } from "../redux/hooks";
 import { IUser } from "../types";
 
 function Signup() {
-    const { register, handleSubmit } = useForm<IUser>();
-    const [registerUser, { isLoading, isError, error, isSuccess, }] =
-        useRegisterUserMutation();
-    const [authLoading, setAuthLoading] = useState(false);
-    const { user } = useAppSelector(state => state.userState)
+  const { register, handleSubmit } = useForm<IUser>();
+  const [registerUser, { isLoading, isError, error, isSuccess }] =
+    useRegisterUserMutation();
+  const [authLoading, setAuthLoading] = useState(false);
+  const { user } = useAppSelector((state) => state.userState);
 
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const from = ((location.state as any)?.from.pathname as string) || '/';
+  const from = ((location.state as any)?.from.pathname as string) || "/";
 
+  const handelRegister = async (data: IUser): Promise<void> => {
+    setAuthLoading(true);
+    try {
+      const { data: res } = (await registerUser(data)) as { data: any };
+      console.log(res, "ree");
 
-    const handelRegister = async (data: IUser): Promise<void> => {
+      const cookie = res?.data?.refreshToken;
+      if (cookie) {
+        Cookies.set("refreshToken", cookie, { expires: 365 });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setAuthLoading(false);
+    }
+    console.log(data, "data");
+  };
 
-        setAuthLoading(true);
-        try {
-            const { data: res } = await registerUser(data) as { data: any };
-            console.log(res, "ree");
+  useEffect(() => {
+    if (isSuccess && !isLoading) {
+      toast.success("You successfully signup ");
 
-            const cookie = res?.data?.refreshToken;
-            if(cookie){
-                Cookies.set("refreshToken", cookie, { expires: 365 });
-            
-            }
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setAuthLoading(false);
-        }
-        console.log(data, 'data');
+      navigate(from);
 
-    };
+      // Reload the page after successful signup
+      window.location.reload();
+    }
+    if (isError) {
+      if (Array.isArray((error as any).data.error)) {
+        (error as any).data.error.forEach((el: any) =>
+          toast.error(el.message, {
+            position: "top-right",
+          })
+        );
+      } else {
+        toast.error((error as any).data.message, {
+          position: "top-right",
+        });
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoading]);
 
-    useEffect(() => {
-
-        if (isSuccess && !isLoading) {
-            toast.success('You successfully signup ');
-
-            navigate(from);
-
-              // Reload the page after successful signup
-              window.location.reload();
-        }
-        if (isError) {
-            if (Array.isArray((error as any).data.error)) {
-                (error as any).data.error.forEach((el: any) =>
-                    toast.error(el.message, {
-                        position: 'top-right',
-                    })
-                );
-            } else {
-                toast.error((error as any).data.message, {
-                    position: 'top-right',
-                });
-            }
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isLoading]);
-
-    return (
-        <div className="flex  h-screen items-center justify-center bg-gray-100 py-10">
-            <form
-                onSubmit={handleSubmit(handelRegister)}
-                className="mx-6 flex w-full flex-col space-y-6 rounded-lg border bg-white px-7 py-10 shadow-lg lg:w-2/5"
-            >
-                {/* logo */}
-                <div className="mx-auto w-44">
-                    <img className="mx-auto" src={logo} alt="logo" />
-                </div>
-
-                {/* {authLoading && (
-                        <div>
-                            <CircleLoader />
-                        </div>
-                    )} */}
-
-                <>
-                    <input
-                        className="rounded-lg border-gray-300 pl-3  py-4 text-sm shadow transition hover:shadow-lg"
-                        type="text"
-                        required
-                        placeholder="Name"
-                        {...register("name")}
-                    />
-                    <input
-                        className="rounded-lg border-gray-300 pl-3  py-4 text-sm shadow transition hover:shadow-lg"
-                        type="email"
-                        required
-                        placeholder="Email"
-                        {...register("email")}
-                    />
-                    <input
-                        className="rounded-lg border-gray-300 pl-3 py-4  text-sm shadow transition hover:shadow-lg"
-                        type="password"
-                        required
-                        placeholder="Password"
-                        {...register("password")}
-                    />
-
-                 { isLoading ? <p>Loading...</p> :
-                   <button className="bg-blue-800 p-3 rounded-sm" type="submit">
-                        Sign up
-                    </button>}
-                </>
-
-                {/* already registered */}
-                <p className="text-center text-lg font-semibold">
-                    already registered ?{" "}
-                    <Link to="/login">
-                        <a className="text-blue-500">Login</a>
-                    </Link>
-                </p>
-            </form>
+  return (
+    <div className="flex items-center justify-center h-screen w-full bg-[#F9F6EA]">
+      <form
+        onSubmit={handleSubmit(handelRegister)}
+        className="flex w-full max-w-[500px] flex-col p-6 my-24 sm:my-6 sm:p-3"
+      >
+        {/* logo */}
+        <div className="mx-auto">
+          <p className="primary-color text-[22px] sm:text-[20px] md:text-[22px] font-medium text-center mb-2">
+            Hello new comers,
+          </p>
+          <div className=" w-full">
+            <img className="mx-auto" src={logo} alt="logo" />
+          </div>
+          {/* <p className="tertiary-color text-[25px] font-medium  text-center mb-[20px] mt-[15px]">
+              Sign In
+            </p> */}
         </div>
-    );
+
+        {/* <div className="bg-gray-200 rounded-lg p-3 mb-6">
+            <p className="font-semibold">Default Login Credentials:</p>
+            <p>Email: admin@admin.com</p>
+            <p>Password: 123456</p>
+          </div> */}
+
+        {authLoading && (
+          <div>
+            <p>Loading...</p>
+          </div>
+        )}
+
+        <div className="flex flex-col space-y-3 my-[20px]">
+          <div className="flex flex-col">
+            <label
+              htmlFor=""
+              className="pl-5 pb-1 tertiary-color text-lg font-medium"
+            >
+              Enter your name
+            </label>
+            <input
+              className="rounded-[25px] bg-[#EEE9CE] pl-3 py-[10px] text-md shadow transition duration-200 ease-in-out hover:shadow-lg focus:outline-none focus:ring-2 focus:tertiary-color"
+              type="text"
+              required
+              {...register("name")}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor=""
+              className="pl-5 pb-1 tertiary-color text-lg font-medium"
+            >
+              Email
+            </label>
+            <input
+              className="rounded-[25px] bg-[#EEE9CE] pl-3 py-[10px] text-md shadow transition duration-200 ease-in-out hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-tertiary-color"
+              type="email"
+              required
+              {...register("email")}
+            />
+          </div>
+          <div className="flex flex-col pb-5">
+            <label
+              htmlFor=""
+              className="pl-5 pb-1 tertiary-color text-lg font-medium"
+            >
+              Password
+            </label>
+            <input
+              className="rounded-[25px] bg-[#EEE9CE] pl-3 py-[10px] text-md shadow transition duration-200 ease-in-out hover:shadow-lg focus:outline-none focus:ring-2 focus:tertiary-color"
+              type="password"
+              required
+              {...register("password")}
+            />
+          </div>
+          <div className="w-full text-center">
+            <button
+              className="p-3 w-[235px] rounded-full mx-auto text-white bg-main font-medium transition duration-200 ease-in-out hover:bg-[#8b7414] focus:outline-none focus:ring-2 focus:ring-[#8b7414]"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+        {/* already registered */}
+        {/* {!verify.success && (
+                          <p className="text-center text-sm font-semibold">
+                              Already have an account ?{" "}
+                              <Link href="/login">
+                                  <a className="text-blue-500">Login</a>
+                              </Link>
+                          </p>
+                      )} */}
+        {/* <div> */}
+        <p className="text-center text-lg font-medium tertiary-color mb-3">
+          Alread member of oceanLibrary?{" "}
+        </p>
+        <Link to="/login" className="block w-full mx-auto text-center">
+          <button className="w-[200px] p-3 rounded-full border font-medium border-[#8b7414] secondary-color mx-auto">
+            Sign up
+          </button>
+        </Link>
+        {/* </div> */}
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
