3c004f821be86ff42c1dc6ff8d798cffbeef0b77
awalhossain <waqin21@gmail.com>
2024-04-25 02:51:37 +0600

    Update routes.tsx file with new routes and components

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,3 +1,4 @@
+import AuthLayout from "@/layout/AuthLayout";
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import UpdateBook from "../components/UpdateBook";
@@ -11,67 +12,73 @@ import Signup from "../pages/Signup";
 import SingleBook from "../pages/SingleBook";
 import { Wishlist } from "../pages/Wishlist";
 
-
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
         path: "/",
-        element: <App />,
-        children: [
-            {
-                path: "/",
-                element: (
-                    <Home />
-                )
-            },
-            {
-                path: "/login",
-                element: <Login />,
-            },
-            {
-                path: "/signup",
-                element: <Signup />,
-            },
-            {
-                path: "/books", element:
-  
-                        <Books />
-            },
-            {
-                path: "/book-details/:id", element:
-                        <SingleBook />
-   
-            },
-            {
-                path: "/update-book/:bookId", element:
-                        <UpdateBook />
-   
-            },
-            {
-                path: "/wishlist", element:
-                    <AuthenticatedLayout allowedRoles={['user', 'admin']}>
-                        <Wishlist />
-                    </AuthenticatedLayout>
-            },
-            {
-                path: "/readinglist", element:
-                    <AuthenticatedLayout allowedRoles={['user', 'admin']}>
-                        <ReadlingList />
-                    </AuthenticatedLayout>
-            },
-            {
-                path: "/addbook", element:
-                    <AuthenticatedLayout allowedRoles={['user', 'admin']}>
-                        <AddBook />
-                    </AuthenticatedLayout>
-            },
-        ]
-    },
-    {
-        path: "*",
-        element: <h1>Not Found</h1>
-    }
-])
-
-
+        element: <Home />,
+      },
+      {
+        path: "/books",
+        element: <Books />,
+      },
+      {
+        path: "/book-details/:id",
+        element: <SingleBook />,
+      },
+      {
+        path: "/update-book/:bookId",
+        element: <UpdateBook />,
+      },
+      {
+        path: "/wishlist",
+        element: (
+          <AuthenticatedLayout allowedRoles={["user", "admin"]}>
+            <Wishlist />
+          </AuthenticatedLayout>
+        ),
+      },
+      {
+        path: "/readinglist",
+        element: (
+          <AuthenticatedLayout allowedRoles={["user", "admin"]}>
+            <ReadlingList />
+          </AuthenticatedLayout>
+        ),
+      },
+      {
+        path: "/addbook",
+        element: (
+          <AuthenticatedLayout allowedRoles={["user", "admin"]}>
+            <AddBook />
+          </AuthenticatedLayout>
+        ),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: (
+      <AuthLayout>
+        <Login />
+      </AuthLayout>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <AuthLayout>
+        <Signup />
+      </AuthLayout>
+    ),
+  },
+  {
+    path: "*",
+    element: <h1>Not Found</h1>,
+  },
+]);
 
 export default router;
