cb891e8d9bb57c9f8c330a4de555d72ffa1cd495
awalhossain <waqin21@gmail.com>
2024-04-28 04:02:49 +0600

    Update main.tsx with global variable definition and import react-quill styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
+// Define global if it doesn't exist
+if (typeof global === "undefined") {
+  window.global = window;
+}
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import "react-quill/dist/quill.snow.css";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import AuthMiddleware from "./helpers/AuthMiddleware";
@@ -7,12 +14,11 @@ import "./index.css";
 import store from "./redux/store";
 import router from "./router/routes";
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <AuthMiddleware>
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
       </AuthMiddleware>
     </Provider>
   </React.StrictMode>
