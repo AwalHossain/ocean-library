57ed9c4944a96859880cca3ef4c92e239c8ec328
awalhossain <waqin21@gmail.com>
2024-04-28 19:37:43 +0600

    Add ReviewUser component to display user information in reviews

---
+++ src/components/reviews/ReviewUser.tsx
@@ -0,0 +1,18 @@
+function ReviewUser() {
+  return (
+    <>
+      {" "}
+      <section className="w-[3.6rem] h-[3.6rem]">
+        <div></div>
+        <img
+          className="rounded-full"
+          src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/users/1442940975i/83582._UX200_CR0,42,200,200_.jpg"
+          alt=""
+        />
+      </section>
+      <p className="font-semibold text-[1.1rem] ml-2">{`Lucinda Berry`}</p>
+    </>
+  );
+}
+
+export default ReviewUser;
