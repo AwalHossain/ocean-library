da06419e989637f99812712004bf4fb8e466c35b
awalhossain <waqin21@gmail.com>
2024-04-28 19:38:34 +0600

    Refactor component names and update styles

--- src/pages/SingleBook.tsx
+++ src/pages/SingleBook.tsx
@@ -1,110 +1,148 @@
 // BookDetails.tsx
 
-import { useEffect, useState } from 'react';
-import { FaPencil, FaTrash } from 'react-icons/fa6';
-import { useNavigate, useParams } from 'react-router-dom';
-import DeleteModal from '../components/DeleteModal';
-import Reviews from '../components/Reviews';
-import Container from '../components/container';
-import { useGetSingleBookQuery } from '../redux/feature/filter/filterApi';
-import { useAppSelector } from '../redux/hooks';
-
+import BookMetadata from "@/components/booksCard/BookMetadata";
+import { CarouselSize } from "@/components/booksCard/Carousel";
+import UserReviews from "@/components/reviews/Reviews";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import DeleteModal from "../components/DeleteModal";
+import { useGetSingleBookQuery } from "../redux/feature/filter/filterApi";
+import { useAppSelector } from "../redux/hooks";
 
 const SingleBook = () => {
-  const { id } = useParams()
-  const { data: book, isLoading, isSuccess, isFetching,refetch } = useGetSingleBookQuery(id)
+  const { id } = useParams();
+  const {
+    data: book,
+    isLoading,
+    isSuccess,
+    isFetching,
+    refetch,
+  } = useGetSingleBookQuery(id);
   const [showModal, setShowModal] = useState(false);
 
   const navigate = useNavigate();
-  const { user } = useAppSelector(state => state.userState)
+  const { user } = useAppSelector((state) => state.userState);
   const athorizedUser = user?.email && book?.addedBy === user.email;
-  
-    
-    // refetch()
-    useEffect(() => {
-      refetch()
-    },[showModal])
- 
-  
-  return (
-    book && isSuccess ? (
-      <>
-        <Container className='flex justify-center'>
-          <div className="flex flex-col lg:flex-row gap-6 mb-8">
-            <div className="">
-              <img
-                className="h-80 rounded-sm"
-                src={book?.thumbnail}
-                alt=""
-              />
+
+  // refetch()
+  useEffect(() => {
+    refetch();
+  }, [showModal]);
+
+  const meta = {
+    title: book?.title ?? "No Title",
+    author: book?.author ?? "No Author",
+  };
+
+  return book && isSuccess ? (
+    <>
+      <div
+        className="grid grid-cols-1 md:grid-cols-12 md:gap-4 justify-center relative"
+        style={{
+          fontFamily: 'Copernicus, "Libre Baskerville", Georgia, serif',
+        }}
+      >
+        <div className="md:col-span-3 md:relative">
+          <div className="md:sticky md:top-20 max-screen">
+            <img
+              className="h-64 md:h-80 rounded-sm mx-auto"
+              src={book?.thumbnail}
+              alt=""
+            />
+            <div className="block md:hidden">
+              <BookMetadata meta={meta} />
             </div>
-            <div>
-              <h4 className="text-xl text-secondary font-semibold">
-                {book?.title}{" "}
-                <span className="text-sm text-slate-500 font-medium">
-                  by {book?.author}
-                </span>
-              </h4>
-              <span className="badge badge-info">{book?.genre}</span>
-              <p className="text-sm mt-2">
-                <span className="font-semibold">Published On: </span>
-                {book?.publicationYear}
-              </p>
-              {/* {verifiedUser && (
-                <div className="flex items-center gap-x-2 mt-8">
-                    <h4 className="font-semibold">Action Center :</h4>
-                    <button
-                    onClick={() => navigate(`/update-book/${book._id}`)}
-                    className="btn btn-sm bg-cyan-700 tooltip"
-                    data-tip="Update Book"
-                    >
-                    <FaPencil />
-                    </button>
-                    <button
-                    onClick={() => setShowModal(!showModal)}
-                    className="btn btn-sm bg-red-700 tooltip"
-                    data-tip="Delete Book"
-                    >
-                    <FaDeleteLeft className="" />
-                    </button>
-                </div>
-                )} */}
+            <div className="py-5 xl:px-10">
+              <button
+                style={{
+                  fontFamily: '"Montserrat", sans-serif',
+                  fontWeight: "500",
+                }}
+                className="w-full p-3 rounded-full border font-medium bg-page text-white mx-auto"
+              >
+                Write a review
+              </button>
             </div>
-            {
-                athorizedUser &&
-                <div className="flex items-center gap-x-2 mt-8">
-                  <h4 className="font-semibold">Action Center :</h4>
-                  <button
-                    onClick={() => navigate(`/update-book/${book._id}`)}
-                    className="bg-cyan-700 hover:bg-cyan-800 p-2 rounded-full tooltip"
-                    data-tip="Update Book"
-                  >
-                    <FaPencil className="text-white" />
-                  </button>
-                  <button
-                    onClick={() => setShowModal(true)}
-                    className="bg-red-700 hover:bg-red-800 p-2 rounded-full tooltip"
-                    data-tip="Delete Book"
-                  >
-                    <FaTrash className="text-white" />
-                  </button>
-                </div>
-              }
           </div>
-        </Container>
-        <div className='flex justify-center py-5'>
-
-          <Reviews bookId={book._id} />
-          {/* <Review /> */}
         </div>
-        {showModal && <DeleteModal book={book} setShowModal={setShowModal} />}
-      </>
 
-    ) : (
-      isFetching ? <div>Loading...</div>: <h2>No Book Found</h2>
-    )
+        <div className="md:col-span-9 md:overflow-y-auto md:pb-16">
+          <div className="hidden md:block">
+            <BookMetadata meta={meta} />
+          </div>
+          <div className="md:grid md:grid-cols-9 xl:gap-8 px-4 md:px-0">
+            <div className="md:col-span-9 xl:col-span-7">
+              <span className="Formatted text-[14px]">
+                <b>We forgive murderers, not pedophiles.</b> <br />
+                <br />
+                Not since Lionel Shriver brought us{" "}
+                <i>We Need to Talk About Kevin</i>
+                has a writer delved into the complexities of a disturbed
+                mother/son relationship. Until now.
+                <br />
+                <br />
+                Meet Noah—an A-honor roll student, award-winning swimmer, and
+                small-town star destined for greatness. There weren’t any signs
+                that something was wrong until the day he confesses to molesting
+                little girls during swim team practice. He’s sentenced to
+                eighteen months in a juvenile sexual rehabilitation center.
+                <br />
+                <br />
+                His mother, Adrianne, refuses to turn her back on him despite
+                his horrific crimes, but her husband won’t allow Noah back into
+                their home. In a series of shocking and shattering revelations,
+                Adrianne is forced to make the hardest decision of her life.
+                Just how far will she go to protect her son?
+                <br />
+                <br />
+                <i>Saving Noah</i> challenges everything you think you know
+                about teenage sexual offenders. It will keep you up at night
+                long after you've read the last page, questioning beliefs you
+                once thought were true.
+              </span>
+              <div className="my-5">
+                <div className="flex flex-wrap space-x-3 ">
+                  <span className="text-[#707070] font-normal">Genre</span>
+                  <span
+                    className="font-bold border-b-2 border-page text-md text-pa relative group hover:border-transparent"
+                    style={{
+                      transitionProperty: "border-color",
+                      // transitionDuration: "0.5s",
+                      // transitionTimingFunction: "ease-in-out",
+                    }}
+                  >
+                    <span
+                      className="absolute inset-0 border-b-2 border-transparent transition-all duration-500 origin-left scale-x-0 group-hover:scale-x-100 group-hover:border-page"
+                      // style={{ transitionTimingFunction: "ease-in-out" }}
+                    ></span>
+                    Thriller
+                  </span>
+                  <span>AudioBook</span>
+                  <span>Mystery</span>
+                  <span>Horror</span>
+                  <span>Fiction</span>
+                </div>
+              </div>
+            </div>
+            <div className="md:col-span-3"></div>
+          </div>
+          {/* Writing Review & Show Review */}
+          <div className="px-4 xl:px-0">
+            <UserReviews />
+          </div>
+        </div>
+      </div>
+      <div className="container ">
+        <CarouselSize />
+      </div>
 
-  )
+      {showModal && <DeleteModal book={book} setShowModal={setShowModal} />}
+    </>
+  ) : isFetching ? (
+    <div>Loading...</div>
+  ) : (
+    <h2>No Book Found</h2>
+  );
 };
 
 export default SingleBook;
