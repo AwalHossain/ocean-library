113740a35a31bfb8315f3db86e72779862a1fd77
awalhossain <waqin21@gmail.com>
2024-04-28 19:37:25 +0600

    Add Carousel component to display books in a carousel

---
+++ src/components/booksCard/Carousel.tsx
@@ -0,0 +1,74 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { useGetAllbooksQuery } from "@/redux/feature/filter/filterApi";
+import { IBook } from "@/types";
+
+export function CarouselSize() {
+  const { data } = useGetAllbooksQuery(undefined);
+
+  const books: IBook[] = data?.data ?? [];
+
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full"
+    >
+      <CarouselContent>
+        {/* {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6"></CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))} */}
+
+        {books.map((book, index) => (
+          <CarouselItem
+            key={index}
+            className="basis-1/1 sm:basis-1/2 md:basis-1/3 lg:basis-1/4  xl:basis-1/5 w-full mx-auto"
+          >
+            <div className="w-auto h-[305px]">
+              <img
+                src={book.thumbnail}
+                alt={book.title}
+                className=" rounded-md w-full h-full object-contain"
+              />
+            </div>
+            <div className="mt-4 text-center ">
+              <h3 className="text-[1rem] font-semibold text-gray-900">
+                {book.title}
+              </h3>
+              <p className="text-[0.9rem] font-medium text-gray-900">
+                {book.author}
+              </p>
+              <div className="flex justify-center items-center space-x-1">
+                {Array.from({ length: book.rating }, (_, index) => (
+                  <svg
+                    key={index}
+                    className="h-4 w-4 text-yellow-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                ))}
+              </div>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
