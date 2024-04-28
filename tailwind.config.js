fad467f1c84b19a21647560658ae181bedc2302e
awalhossain <waqin21@gmail.com>
2024-04-28 19:36:18 +0600

    Add page color variable to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        page: "var(--page-color)",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
