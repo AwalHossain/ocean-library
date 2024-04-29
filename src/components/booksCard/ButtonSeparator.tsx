5bc2f9fae5fa83c38b6d6bbb6dfcf79590d7bf16
awalhossain <waqin21@gmail.com>
2024-04-30 00:11:22 +0600

    Refactor ButtonWithSeparator component to import BookModal and update button text in ButtonSeparator.tsx

--- src/components/booksCard/ButtonSeparator.tsx
+++ src/components/booksCard/ButtonSeparator.tsx
@@ -1,3 +1,5 @@
+import { BookModal } from "../card/BookModal";
+
 const ButtonWithSeparator = () => {
   const onLeftClick = () => {
     console.log("left button clicked");
@@ -6,20 +8,22 @@ const ButtonWithSeparator = () => {
     console.log("right button clicked");
   };
   return (
-    <div className="flex items-center rounded-full bg-green-500 text-white font-semibold w-full">
+    <div
+      className="flex items-center rounded-full bg-page text-white font-semibold w-full"
+      style={{
+        fontFamily: '"Montserrat", sans-serif',
+        fontWeight: "500",
+      }}
+    >
       <button
         onClick={onLeftClick}
         className="px-4 py-2 rounded-l-full hover:bg-green-600 transition-colors duration-300 w-[80%]"
       >
-        leftText
+        want to read
       </button>
-      <span className="h-6 border-l border-white"></span>
-      <button
-        onClick={onRightClick}
-        className="px-4 py-2 rounded-r-full bg-white text-gray-700 border border-gray-300 hover:bg-gray-200 transition-colors duration-300 w-[20%]"
-      >
-        right
-      </button>
+      <span className="h-10 border-l border-white"></span>
+
+      <BookModal />
     </div>
   );
 };
