ab05e33bf0e553a55b74f2bf6fe2c8c72f743311
awalhossain <waqin21@gmail.com>
2024-04-30 00:17:21 +0600

    Refactor SortBooks component to add sorting functionality in SortBooks.tsx

---
+++ src/components/browseBooks/SortBooks.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import { Command, CommandGroup, CommandItem } from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+
+const sortOptions = [
+  {
+    value: "asc",
+    label: "Asc",
+  },
+  {
+    value: "desc",
+    label: "Desc",
+  },
+];
+
+export default function SortBooks() {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-full max-w-[150px] justify-between"
+        >
+          {value
+            ? sortOptions.find((framework) => framework.value === value)?.label
+            : "Sort by"}
+          <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandGroup>
+            {sortOptions.map((framework) => (
+              <CommandItem
+                key={framework.value}
+                value={framework.value}
+                onSelect={(currentValue) => {
+                  setValue(currentValue === value ? "" : currentValue);
+                  setOpen(false);
+                }}
+              >
+                {framework.label}
+                <CheckIcon
+                  className={cn(
+                    "ml-auto h-4 w-4",
+                    value === framework.value ? "opacity-100" : "opacity-0"
+                  )}
+                />
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
