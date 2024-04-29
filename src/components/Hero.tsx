28bc3fb18ebd5e84d21c5feca30fd15c5547e608
awalhossain <waqin21@gmail.com>
2024-04-29 14:04:43 +0600

    Update Hero component styles and content

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,13 +3,13 @@ import bookImg from "@/assets/hero.svg";
 const Hero = () => {
   return (
     <>
-      <div className="flex flex-wrap justify-center items-center">
+      <div className="flex flex-wrap justify-center items-center flex-col-reverse md:flex-row">
         <div className="flex items-center w-full lg:w-1/2">
-          <div className="md:pl-10 mb-8">
-            <h1 className="text-[36px] font-bold leading-snug tracking-tight secondary-color lg:leading-tight xl:leading-tight dark:text-white">
+          <div className="md:pl-2 mb-8">
+            <h1 className="text-[36px] hidden md:block font-bold leading-snug tracking-tight secondary-color lg:leading-tight xl:leading-tight dark:text-white">
               Discover New Books for Every Taste
             </h1>
-            <p className="py-5 text-[20px] leading-normal secondary-color dark:text-gray-300">
+            <p className="py-5 text-[12px] sm:text-[18px] leading-normal secondary-color dark:text-gray-300">
               Explore our vast collection of books, from classic literature to
               the latest bestsellers. Whether you love fiction, non-fiction, or
               poetry, we have something for every book lover.
@@ -28,10 +28,31 @@ const Hero = () => {
           </div>
         </div>
         <div className="lg:w-1/2">
-          <div className="w-[550px] lg:w-auto justify-center">
+          <div className="w-auto lg:w-auto justify-center">
+            <h1 className="text-[40px] md:hidden font-bold leading-snug tracking-tight secondary-color lg:leading-tight xl:leading-tight dark:text-white">
+              Discover New{" "}
+              <span
+                style={{
+                  fontFamily: "Copernicus, 'Libre Baskerville', Georgia, serif",
+                }}
+                className="italic font-bold text-main"
+              >
+                Books
+              </span>
+              <br />
+              For Every
+              <span
+                style={{
+                  fontFamily: "Copernicus, 'Libre Baskerville', Georgia, serif",
+                }}
+                className="italic font-bold pl-2"
+              >
+                Taste
+              </span>
+            </h1>
             <img
               src={bookImg}
-              className={"object-cover"}
+              className="w-full h-full object-cover"
               alt="Book Illustration"
               loading="eager"
               placeholder="blur"
