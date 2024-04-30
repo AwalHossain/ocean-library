849ef082e3796e586921e16d52b743bfc2b28458
awalhossain <waqin21@gmail.com>
2024-05-01 04:31:50 +0600

    Update API configuration in apiSlice.ts

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,27 +1,33 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import Cookies from "js-cookie";
 
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import Cookies from 'js-cookie';
+const cookie = Cookies.get("refreshToken");
 
+const BASE_URL = process.env.REACT_APP_API_URL;
 
-
-const cookie = Cookies.get('refreshToken')
-
-const BASE_URL = "https://bookend-awalho.vercel.app/api/v1"
+console.log(BASE_URL, "BASE_URL", process.env.REACT_APP_API_URL);
 
 export const api = createApi({
-    reducerPath: 'api',
-    baseQuery: fetchBaseQuery({
-        baseUrl: BASE_URL,
-        credentials: "include",
-        prepareHeaders: (headers, { }) => {
-          // const token = cookie
-          if (cookie) {
-           // include token in req header
-            headers.set('authorization', `${cookie}`)  
-            return headers
-          }
-        }
-      }),
-     tagTypes: ["Books", "Book", "Reviews", "Wishlist", "finishedList","readingList"],
-    endpoints:()=>({})
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    credentials: "include",
+    prepareHeaders: (headers, {}) => {
+      // const token = cookie
+      if (cookie) {
+        // include token in req header
+        headers.set("authorization", `${cookie}`);
+        return headers;
+      }
+    },
+  }),
+  tagTypes: [
+    "Books",
+    "Book",
+    "Reviews",
+    "Wishlist",
+    "finishedList",
+    "readingList",
+  ],
+  endpoints: () => ({}),
 });
