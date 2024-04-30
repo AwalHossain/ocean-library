7f67864a122b3da9b2aa0feaf1e4ecfb14927192
awalhossain <waqin21@gmail.com>
2024-05-01 04:28:09 +0600

    Add dotenv configuration to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 import path from "path";
 import { defineConfig } from "vite";
+dotenv.config(); // load env vars from .env
 
 // https://vitejs.dev/config/
 export default defineConfig({
