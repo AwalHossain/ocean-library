2bfa18b07ddb2001aa57129e000d747b21f65449
awalhossain <waqin21@gmail.com>
2024-05-01 04:32:32 +0600

    Refactor bookSlice.ts to update userPreference field and remove unused code

--- src/redux/feature/book/bookSlice.ts
+++ src/redux/feature/book/bookSlice.ts
@@ -1,39 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { IBook } from "../../api/types";
-
-
-
 
 interface IWishlistState {
-    wishlist: IBook[] | null;
-    readingList: IBook[] | null;
-    finishedList: IBook[] | null;
-  } 
+  userPreference: string;
+}
 
- const initialState: IWishlistState = {
-    wishlist: [],
-    readingList: [],
-    finishedList: []
-  }
-  
-  export const bookSlice = createSlice({
-    initialState,
-    name: 'bookSlice',
-    reducers: {
-        setFinishedList: (state, action) => {
-            state.finishedList = action.payload
-        },
-        setWishList : (state, action) => {
-            state.wishlist = action.payload
-        },
-        setReadingList: (state, action) => {
-            state.readingList = action.payload
-        },
+const initialState: IWishlistState = {
+  userPreference: "want to read",
+};
 
-    }
+export const bookSlice = createSlice({
+  initialState,
+  name: "bookSlice",
+  reducers: {
+    setUserPrefernce: (state, action) => {
+      state.userPreference = action.payload;
+    },
+  },
+});
 
-})
+export const { setUserPrefernce } = bookSlice.actions;
 
-export const {setWishList, setReadingList, setFinishedList} = bookSlice.actions
-
-export default bookSlice.reducer
+export default bookSlice.reducer;
