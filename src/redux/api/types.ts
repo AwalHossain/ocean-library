e34f1b6dae284614fd2fae2f3c3d827c8d246bd2
awalhossain <waqin21@gmail.com>
2024-05-01 04:31:58 +0600

    Update types.ts to include userPreference field in IBook and update IReview structure

--- src/redux/api/types.ts
+++ src/redux/api/types.ts
@@ -1,34 +1,37 @@
-
-
-
 export type IUser = {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  _id: string;
+};
+
+export type IBook = {
+  title: string;
+  author: string;
+  thumbnail: string;
+  price: string;
+  rating: string;
+  featured: boolean;
+  genre: string;
+  publicationYear: string;
+  reviews?: IReview[];
+  wishlist?: string[];
+  readingList?: string[];
+  finishedList?: string[];
+  addedBy?: string;
+  userPreference?: {
+    status: string;
+    user: string;
+  }[];
+  _id: string;
+};
+
+export type IReview = {
+  userId?: {
+    email: string;
     name: string;
-    email: string;
-    password: string;
-    role: string;
-    _id: string;
   };
-
-  export type IBook = {
-    title: string;
-    author: string;
-    thumbnail: string;
-    price: string;
-    rating: string;
-    featured: boolean;
-    genre: string;
-    publicationYear: string;
-    reviews?: IReview[];
-    wishlist?: string[];
-    readingList?: string[];
-    finishedList?: string[];
-    addedBy?: string;
-    _id: string;
-  };
-
-
-  export type IReview = {
-    _id: string;
-    email: string;
-    review: string;
-  }
+  rating: number;
+  description: string;
+};
