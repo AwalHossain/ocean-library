51f38349ec17f34994538d9a5269935e16bd4353
awalhossain <waqin21@gmail.com>
2024-05-01 04:32:43 +0600

    Refactor filterApi.ts to update addReview and filterBooks endpoints and remove unused code

--- src/redux/feature/filter/filterApi.ts
+++ src/redux/feature/filter/filterApi.ts
@@ -2,9 +2,6 @@ import { api } from "../../api/apiSlice";
 import { IBook } from "../../api/types";
 import { setBook, setReviews } from "./filterSlice";
 
-
-
-
 const filterApi = api.injectEndpoints({
   endpoints: (builder) => ({
     addReview: builder.mutation({
@@ -13,8 +10,8 @@ const filterApi = api.injectEndpoints({
           url: `book/review/${data.bookId}`,
           method: "PATCH",
           credentials: "include",
-          body: data
-        }
+          body: data,
+        };
       },
       invalidatesTags: ["Reviews"],
     }),
@@ -26,35 +23,33 @@ const filterApi = api.injectEndpoints({
           credentials: "include",
         };
       },
-      providesTags: ['Reviews', 'Books'],
-      transformResponse: (result: { data: IBook }) =>
-        result.data,
+      providesTags: ["Reviews", "Books"],
+      transformResponse: (result: { data: IBook }) => result.data,
       async onQueryStarted(_args, { dispatch, queryFulfilled }) {
         try {
           const { data } = await queryFulfilled;
 
           dispatch(setReviews(data.reviews));
           // dispatch(setUser(data));
-        } catch (error) { }
+        } catch (error) {}
       },
     }),
     filterBooks: builder.query({
       query(data) {
         return {
-          url: `book/all/${data}&sortBy=${'createdAt'}&sortOrder=${'desc'}`,
+          url: `book/all/${data}&sortBy=${"createdAt"}&sortOrder=${"desc"}`,
           credentials: "include",
         };
       },
-      providesTags: ['Books'],
-      transformResponse: (result: { data: { data: IBook[] } }) =>
-        result.data,
+      providesTags: ["Books"],
+      transformResponse: (result: { data: { data: IBook[] } }) => result.data,
       async onQueryStarted(_args, { dispatch, queryFulfilled }) {
         try {
           const { data } = await queryFulfilled;
           dispatch(setBook(data.data));
 
           // dispatch(setUser(data));
-        } catch (error) { }
+        } catch (error) {}
       },
     }),
     editBook: builder.mutation({
@@ -63,9 +58,9 @@ const filterApi = api.injectEndpoints({
           url: `book/${data.bookId}`,
           method: "PATCH",
           credentials: "include",
-          body: data
-        }
-      }
+          body: data,
+        };
+      },
       // invalidatesTags: ["Books"],
     }),
     deleteBook: builder.mutation({
@@ -74,20 +69,19 @@ const filterApi = api.injectEndpoints({
           url: `book/${data}`,
           method: "DELETE",
           credentials: "include",
-        }
+        };
       },
       invalidatesTags: ["Books"],
     }),
     getAllbooks: builder.query({
       query() {
         return {
-          url: `book/all?page=${1}&limit=${10}&sortBy=${'createdAt'}&sortOrder=${'desc'}`,
+          url: `book/all?page=${1}&limit=${10}&sortBy=${"createdAt"}&sortOrder=${"desc"}`,
           credentials: "include",
         };
       },
-      providesTags: ['Books'],
-      transformResponse: (result: { data: { data: IBook[] } }) =>
-        result.data,
+      providesTags: ["Books"],
+      transformResponse: (result: { data: { data: IBook[] } }) => result.data,
       async onQueryStarted(_args, { dispatch, queryFulfilled }) {
         try {
           const { data } = await queryFulfilled;
@@ -95,17 +89,17 @@ const filterApi = api.injectEndpoints({
           dispatch(setBook(data.data));
 
           // dispatch(setUser(data));
-        } catch (error) { }
+        } catch (error) {}
       },
+    }),
+  }),
+});
 
-    })
-
-  })
-
-})
-
-
-export const { useAddReviewMutation,
+export const {
+  useAddReviewMutation,
   useDeleteBookMutation,
   useEditBookMutation,
-  useFilterBooksQuery, useGetAllbooksQuery, useGetSingleBookQuery } = filterApi;
+  useFilterBooksQuery,
+  useGetAllbooksQuery,
+  useGetSingleBookQuery,
+} = filterApi;
