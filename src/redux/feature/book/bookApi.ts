34e6bd76106f19e7e9bd4dd29450718420710cca
awalhossain <waqin21@gmail.com>
2024-05-01 04:32:19 +0600

    Refactor bookApi.ts to update preference endpoint and remove unused code

--- src/redux/feature/book/bookApi.ts
+++ src/redux/feature/book/bookApi.ts
@@ -1,7 +1,5 @@
 import { api } from "../../api/apiSlice";
 import { IBook } from "../../api/types";
-import { setFinishedList, setReadingList, setWishList } from "./bookSlice";
-
 
 const bookApi = api.injectEndpoints({
   endpoints: (builder) => ({
@@ -11,11 +9,11 @@ const bookApi = api.injectEndpoints({
         method: "POST",
         credentials: "include",
         body: data,
-      })
+      }),
     }),
-    addToWishList: builder.mutation({
+    addToPrefernce: builder.mutation({
       query: (data) => ({
-        url: "users/wishlist",
+        url: "book/preference",
         method: "POST",
         credentials: "include",
         body: data,
@@ -29,17 +27,17 @@ const bookApi = api.injectEndpoints({
         method: "GET",
         credentials: "include",
       }),
-      providesTags: ['Wishlist'],
-      transformResponse: (result: { data: { wishlist: IBook[] } }) =>
-        result.data,
-      async onQueryStarted(_args, { dispatch, queryFulfilled }) {
-        try {
-          const { data } = await queryFulfilled;
-          dispatch(setWishList(data.wishlist));
+      providesTags: ["Wishlist"],
+      // transformResponse: (result: { data: { wishlist: IBook[] } }) =>
+      //   result.data,
+      // async onQueryStarted(_args, { dispatch, queryFulfilled }) {
+      //   try {
+      //     const { data } = await queryFulfilled;
+      //     dispatch(setWishList(data.wishlist));
 
-          // dispatch(setUser(data));
-        } catch (error) { }
-      },
+      //     // dispatch(setUser(data));
+      //   } catch (error) {}
+      // },
     }),
 
     removeFromWishList: builder.mutation({
@@ -67,17 +65,17 @@ const bookApi = api.injectEndpoints({
         method: "GET",
         credentials: "include",
       }),
-      providesTags: ['readingList'],
+      providesTags: ["readingList"],
       transformResponse: (result: { data: { readingList: IBook } }) =>
         result.data,
-      async onQueryStarted(_args, { dispatch, queryFulfilled }) {
-        try {
-          const { data } = await queryFulfilled;
-          dispatch(setReadingList(data.readingList));
+      // async onQueryStarted(_args, { dispatch, queryFulfilled }) {
+      //   try {
+      //     const { data } = await queryFulfilled;
+      //     dispatch(setReadingList(data.readingList));
 
-          // dispatch(setUser(data));
-        } catch (error) { }
-      },
+      //     // dispatch(setUser(data));
+      //   } catch (error) {}
+      // },
     }),
 
     removeFromReadingList: builder.mutation({
@@ -88,7 +86,6 @@ const bookApi = api.injectEndpoints({
       }),
       invalidatesTags: ["readingList"],
     }),
-
 
     addToFinishedList: builder.mutation({
       query: (data) => ({
@@ -105,25 +102,27 @@ const bookApi = api.injectEndpoints({
         method: "GET",
         credentials: "include",
       }),
-      providesTags: ['finishedList'],
+      providesTags: ["finishedList"],
       transformResponse: (result: { data: { finishedBooks: IBook } }) =>
-      result.data,
-    async onQueryStarted(_args, { dispatch, queryFulfilled }) {
-      try {
-        const { data } = await queryFulfilled;
-        dispatch(setFinishedList(data.finishedBooks));
-      } catch (error) { }
-    },
-    })
-  })
-})
-
+        result.data,
+      // async onQueryStarted(_args, { dispatch, queryFulfilled }) {
+      //   try {
+      //     const { data } = await queryFulfilled;
+      //     dispatch(setFinishedList(data.finishedBooks));
+      //   } catch (error) {}
+      // },
+    }),
+  }),
+});
 
 export const {
   useAddBookMutation,
-  useAddToWishListMutation, useGetWishListQuery, useAddToReadingListMutation, useGetReadingListQuery,
-  useAddToFinishedListMutation, useGetFinishedListQuery,
-  useRemoveFromReadingListMutation, useRemoveFromWishListMutation
+  useAddToPrefernceMutation,
+  useGetWishListQuery,
+  useAddToReadingListMutation,
+  useGetReadingListQuery,
+  useAddToFinishedListMutation,
+  useGetFinishedListQuery,
+  useRemoveFromReadingListMutation,
+  useRemoveFromWishListMutation,
 } = bookApi;
-
-
