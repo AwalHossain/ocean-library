38597473d57946f3f35a49b98624c4b83177d3ea
awalhossain <waqin21@gmail.com>
2024-05-01 17:40:59 +0600

    Refactor HoverContent component to update user preference and add loading state

--- src/components/card/hoverContent.tsx
+++ src/components/card/hoverContent.tsx
@@ -5,46 +5,70 @@ import * as React from "react";
 
 import { Command, CommandGroup, CommandItem } from "@/components/ui/command";
 import { cn } from "@/lib/utils";
+import { useAddToPrefernceMutation } from "@/redux/feature/book/bookApi";
+import { setUserPrefernce } from "@/redux/feature/book/bookSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 
-const frameworks = [
+const dataObj = [
   {
-    value: "want to read",
-    label: "Want to Read",
+    value: "read",
+    label: "Read",
   },
   {
-    value: "currently reading",
+    value: "reading",
     label: "Currently Reading",
   },
-  {
-    value: "wishlist",
-    label: "Wishlist",
-  },
 ];
-
-export function HoverContent() {
+export function HoverContent({
+  bookId,
+  setUpdatingBookId,
+}: {
+  bookId: string;
+  setUpdatingBookId: (id: string | null) => void;
+}) {
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState("");
+  const [loadingItem, setLoadingItem] = React.useState<string | null>(null);
+
+  const [addToPrefernce, { isLoading, error, data }] =
+    useAddToPrefernceMutation();
+
+  const { user } = useAppSelector((state) => state.userState);
+  const { userPreference } = useAppSelector((state) => state.userBookState);
+  const dispatch = useAppDispatch();
 
   return (
     <Command className="">
       <CommandGroup className="p-0 m-0 ">
-        {frameworks.map((framework) => (
+        {dataObj.map((item) => (
           <CommandItem
-            key={framework.value}
-            value={framework.value}
-            onSelect={(currentValue) => {
+            key={item.value}
+            value={item.value}
+            onSelect={async (currentValue) => {
               setValue(currentValue === value ? "" : currentValue);
               setOpen(false);
+              dispatch(setUserPrefernce(item.label));
+              setLoadingItem(item.value);
+              setUpdatingBookId(bookId);
+              await addToPrefernce({ status: item.value, bookId });
+              setLoadingItem(null);
+              setUpdatingBookId(null);
             }}
             className="text-[13px] p-0 m-0! cursor-pointer aria-selected:bg-slate-400"
           >
-            <Check
-              className={cn(
-                "mr-2 h-4 w-4",
-                value === framework.value ? "opacity-100" : "opacity-0"
-              )}
-            />
-            {framework.label}
+            {loadingItem === item.value ? (
+              <span className="w-40">Loading...</span>
+            ) : (
+              <>
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    value === item.value ? "opacity-100" : "opacity-0"
+                  )}
+                />
+                {item.label}
+              </>
+            )}
           </CommandItem>
         ))}
       </CommandGroup>
