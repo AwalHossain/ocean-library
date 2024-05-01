be192319567599cebac05644a174aa60061e6467
awalhossain <waqin21@gmail.com>
2024-05-01 17:40:52 +0600

    Refactor HoverChevron component to accept bookId and setUpdatingBookId props

--- src/components/card/hoverChevron.tsx
+++ src/components/card/hoverChevron.tsx
@@ -7,19 +7,26 @@ import { ChevronDown } from "lucide-react";
 import React from "react";
 import { HoverContent } from "./hoverContent";
 
-export function HoverChevron() {
+export function HoverChevron({
+  bookId,
+  setUpdatingBookId,
+}: {
+  bookId: string;
+  setUpdatingBookId: (id: string | null) => void;
+}) {
   const [isOpen, setIsOpen] = React.useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
+
   return (
-    <HoverCard open={isOpen} onOpenChange={setIsOpen}>
+    <HoverCard open={isOpen}>
       <HoverCardTrigger asChild onClick={handleClick}>
         <ChevronDown className="text-white cursor-pointer inline-block w-auto h-auto" />
       </HoverCardTrigger>
       <HoverCardContent className="w-auto max-h-[250px] p-1.5 border-2 border-[#4444444d]">
-        <HoverContent />
+        <HoverContent bookId={bookId} setUpdatingBookId={setUpdatingBookId} />
       </HoverCardContent>
     </HoverCard>
   );
