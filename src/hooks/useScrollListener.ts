14016887d46da2000715daa3fbb952dde9c5223a
awalhossain <waqin21@gmail.com>
2024-05-04 23:21:39 +0600

    Add useScrollListener hook for handling scroll events in the UI

---
+++ src/hooks/useScrollListener.ts
@@ -0,0 +1,33 @@
+import { createContext, useEffect, useState } from "react";
+
+export default function useScrollListener() {
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    lastX: 0,
+    lastY: 0,
+  });
+
+  // set up the event listener
+  useEffect(() => {
+    const handleScroll = () => {
+      setData((last) => {
+        return {
+          x: window.scrollX,
+          y: window.scrollY,
+          lastX: last.x,
+          lastY: last.y,
+        };
+      });
+    };
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return data;
+}
+
+export const scrollContext = createContext(null);
