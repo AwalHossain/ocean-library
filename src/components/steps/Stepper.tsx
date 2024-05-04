374530e0df2b21524a1e6f54ebc0d68bae228249
awalhossain <waqin21@gmail.com>
2024-05-04 23:18:24 +0600

    Add Stepper component for displaying a progress indicator in the form

---
+++ src/components/steps/Stepper.tsx
@@ -0,0 +1,54 @@
+import { CheckIcon } from "lucide-react";
+import React from "react";
+
+interface StepperProps {
+  currentStep: number;
+  numOfSteps: number;
+  isSubmitted: boolean; // Add this prop
+}
+
+export default function Stepper({
+  currentStep,
+  numOfSteps,
+  isSubmitted,
+}: StepperProps) {
+  return (
+    <div className="flex items-center">
+      {Array.from({ length: numOfSteps }, (_, i) => {
+        const index = i + 1;
+        const isActive = index === currentStep;
+        const isCompleted =
+          index <= currentStep || (index === numOfSteps && isSubmitted);
+
+        return (
+          <React.Fragment key={index}>
+            <div className="flex justify-center items-center">
+              <div
+                className={`rounded-full border-2 ${
+                  isCompleted
+                    ? "bg-green-400 border-gray-200"
+                    : "border-gray-400"
+                } w-10 h-10 flex justify-center items-center`}
+              >
+                {isCompleted ? (
+                  <span className="text-center">
+                    <CheckIcon size={20} className="text-white" />
+                  </span>
+                ) : (
+                  <span className="text-black text-center">{index}</span>
+                )}
+              </div>
+            </div>
+            {index !== numOfSteps && (
+              <div
+                className={`w-20 h-1 ${
+                  isActive ? "bg-yellow-700" : "bg-gray-400"
+                }`}
+              ></div>
+            )}
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
+}
