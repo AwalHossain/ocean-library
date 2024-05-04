495cba181cc615cc4556af9a866a3e1489381f02
awalhossain <waqin21@gmail.com>
2024-05-04 23:20:24 +0600

    Add useMediaQuery hook for handling media queries in the UI

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function useMediaQuery(query: string) {
+  const [value, setValue] = React.useState(false);
+  React.useEffect(() => {
+    function onChange(event: MediaQueryListEvent) {
+      setValue(event.matches);
+    }
+    const result = matchMedia(query);
+    result.addEventListener("change", onChange);
+    setValue(result.matches);
+    return () => result.removeEventListener("change", onChange);
+  }, [query]);
+  return value;
+}
