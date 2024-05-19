eec81f731ddcfcba88fd0543bde14a0202626acf
awalhossain <waqin21@gmail.com>
2024-05-19 15:55:46 +0600

    feat: Add WriteModal component for creating a new book entry

--- src/components/steps/StepOne.tsx
+++ src/components/steps/StepOne.tsx
@@ -1,5 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
 import {
   Form,
   FormControl,
@@ -10,15 +8,14 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { cn } from "@/lib/utils";
-import { format } from "date-fns";
-import { CalendarIcon } from "lucide-react";
 import React from "react";
-import { UseFormReturn } from "react-hook-form";
+import { UseFormReturn, useController } from "react-hook-form";
+import ReactQuill from "react-quill";
 import { z } from "zod";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Textarea } from "../ui/textarea";
+
 const accountFormSchema = z.object({
-  name: z
+  title: z
     .string()
     .min(2, {
       message: "Name must be at least 2 characters.",
@@ -26,11 +23,20 @@ const accountFormSchema = z.object({
     .max(30, {
       message: "Name must not be longer than 30 characters.",
     }),
-  dob: z.date({
-    required_error: "A date of birth is required.",
+  // dob: z.date({
+  //   required_error: "A date of birth is required.",
+  // }),
+  author: z.string({
+    required_error: "Please enter an author name.",
   }),
-  language: z.string({
-    required_error: "Please select a language.",
+  description: z.string({
+    required_error: "Please enter a description.",
+  }),
+  summary: z.string({
+    message: "Please enter a summary.",
+  }),
+  thumbnail: z.string({
+    required_error: "Please enter a thumbnail url",
   }),
 });
 
@@ -38,81 +44,145 @@ type AccountFormValues = z.infer<typeof accountFormSchema>;
 
 interface StepOneProps {
   form: UseFormReturn<AccountFormValues>;
-  onSubmit: (data: AccountFormValues) => void;
+  onSubmit?: (data: AccountFormValues) => void;
   handleNextStep: () => void;
 }
 
-const StepOne: React.FC<StepOneProps> = ({
-  form,
-  onSubmit,
-  handleNextStep,
-}) => {
+const StepOne: React.FC<StepOneProps> = ({ form, handleNextStep }) => {
+  const {
+    field: { value, onChange },
+  } = useController({
+    name: "description",
+    control: form.control,
+  });
+
+  const modules = {
+    toolbar: [
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }],
+    ],
+  };
+
+  const formats = [
+    // "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "font",
+  ];
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+      <form>
         <FormField
           control={form.control}
-          name="name"
+          name="title"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Name</FormLabel>
+              <FormLabel className="text-sm font-semibold text-gray-700">
+                Title
+              </FormLabel>
               <FormControl>
-                <Input placeholder="Your name" {...field} />
+                <Input placeholder="Book name" {...field} required />
               </FormControl>
-              <FormDescription>
+              {/* <FormDescription>
                 This is the name that will be displayed on your profile and in
                 emails.
-              </FormDescription>
+              </FormDescription> */}
               <FormMessage />
             </FormItem>
           )}
         />
         <FormField
           control={form.control}
-          name="dob"
+          name="author"
           render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Date of birth</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-[240px] pl-3 text-left font-normal",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      {field.value ? (
-                        format(field.value, "PPP")
-                      ) : (
-                        <span>Pick a date</span>
-                      )}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    disabled={(date) =>
-                      date > new Date() || date < new Date("1900-01-01")
-                    }
-                    initialFocus
-                  />
-                </PopoverContent>
-              </Popover>
+            <FormItem>
+              <FormLabel className="text-sm font-semibold text-gray-700">
+                Author Name
+              </FormLabel>
+              <FormControl>
+                <Input placeholder="Book name" {...field} required />
+              </FormControl>
+              {/* <FormDescription>
+                This is the name that will be displayed on your profile and in
+                emails.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={() => (
+            <FormItem>
+              <FormLabel className="text-sm font-semibold text-gray-700">
+                Description
+              </FormLabel>
+              <FormControl>
+                <ReactQuill
+                  theme="snow"
+                  modules={modules}
+                  formats={formats}
+                  value={value}
+                  onChange={onChange}
+                />
+              </FormControl>
               <FormDescription>
-                Your date of birth is used to calculate your age.
+                This is the description that will be
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <button type="button" onClick={handleNextStep}>
-          Next Step
+
+        <FormField
+          control={form.control}
+          name="summary"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-sm font-semibold text-gray-700">
+                Summary
+              </FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Type your message here."
+                  {...field}
+                  required
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="thumbnail"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-sm font-semibold text-gray-700">
+                Thumbnail
+              </FormLabel>
+              <FormControl>
+                <Input placeholder="Thumbnail URL" {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <button
+          onClick={handleNextStep}
+          className="bg-blue-600 text-white p-2 rounded-md mt-10"
+        >
+          Next step
         </button>
       </form>
     </Form>
